cbb85aa6176092e5c6ba87edbdd9f42207772e5a
Jean-Philippe Moreux <jpmoreux@gmail.com>
2026-02-06 19:36:26 +0100

    Add files via upload

---
+++ Charts/js/highslide.config.js
@@ -0,0 +1,16 @@
+/**
+*	Site-specific configuration settings for Highslide JS
+*/
+hs.graphicsDir = 'https://www.highcharts.com/highslide/graphics/';
+/** hs.outlineType = 'rounded-white'; */
+hs.wrapperClassName = 'draggable-header';
+hs.captionEval = 'this.a.title';
+hs.showCredits = false;
+hs.marginTop = 10;
+hs.marginRight = 10;
+hs.marginBottom = 10;
+hs.marginLeft = 10;
+hs.maxWidth = 80;
+hs.minWidth = 80;
+hs.maxHeight = 40;
+hs.minHeight = 40;
